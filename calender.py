2474f914443a5bc6717ae0dc592d622a0dd48f19
iqrawaqarr <157203849+iqrawaqarr@users.noreply.github.com>
2024-02-18 19:40:10 +0500

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,11 @@
+#iqra and fiza 
+#iqra09930@gmail.com
+#The first step in creating a calendar with Python 
+
+import calendar
+year=int(input("enter the year:"))
+#month=int(input("enter the month:"))
+
+cal=calendar.calendar(year)
+
+print(cal)
